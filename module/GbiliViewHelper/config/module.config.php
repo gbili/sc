f7c82ac67cadd276f68f23aebd4bfe7b88f8128e
Guillermo Devi <g@gbili.com>
2014-04-30 16:51:47 +0100

    updated composer

--- module/GbiliViewHelper/config/module.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace GbiliViewHelper;
-
-return array(
-    //'navigation'         => include __DIR__ . '/navigation.config.php',
-    //'service_manager'    => include __DIR__ . '/service_manager.config.php',
-    'view_helpers'       => include __DIR__ . '/view_helpers.config.php',
-);
